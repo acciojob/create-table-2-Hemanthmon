e1c771b046b393e2571efa715aa511c5ad385c0f
Hemanth V <153289923+Hemanthmon@users.noreply.github.com>
2025-04-25 13:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function createTable() {
     //Write your code here
-  
+    const rows = prompt("Input Number of rows")
+	const columns = prompt("Input Number of columns")
+
+	//convert in to the numbers
+
+	const rowsNum = Number(rows)
+	const cellNum = Number(columns)
+//check points
+	if(isNaN(rowsNum) || isNaN(cellNum) || rowsNum <= 0 || cellNum <= 0 ){
+		alert("Please enter the positive number for the rows and columns")
+		return
+	}
+
+  let table = document.getElementById('myTable')
+
+	for(let i = 0; i < rowsNum; i++){
+		const row = table.insertRow();
+     for(let j = 0; j < cellNum; j++){
+         const cell = row.insertCell()
+
+		 cell.innerText = `Row-${i} column-${j}`
+	 }
+	}
+
+	
+    //now 
 }
